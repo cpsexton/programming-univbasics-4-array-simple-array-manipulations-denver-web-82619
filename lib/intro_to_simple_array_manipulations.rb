acf6a73d0cf9d3dd584dde16795d66faabe2970d
Christopher Sexton <cpsexton@gmail.com>
2019-08-03 20:48:47 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -22,6 +22,7 @@ end
 
 def using_shift(my_favorite_cities)
 my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]  
+
 end
 
 def shift_with_args(array)
