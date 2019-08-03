ceae5579520b79b61cec2bc3c97bd001ea3dce6c
Christopher Sexton <cpsexton@gmail.com>
2019-08-03 21:15:45 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -42,7 +42,7 @@ another_language = "Python"
 list_of_programming_languages.insert(4, another_language)
 end
 
-def using_uniq(array)
+def using_uniq(haircuts)
   haircuts = ["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler", "Bob"]
   haircuts.uniq('bob')
 end
