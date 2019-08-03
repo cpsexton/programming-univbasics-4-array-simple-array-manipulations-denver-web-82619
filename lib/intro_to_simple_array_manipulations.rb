6a33477806187c51c5ccd2fac16c137883eecf66
Christopher Sexton <cpsexton@gmail.com>
2019-08-03 20:52:16 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -7,26 +7,27 @@ end
 def using_unshift(bouroughs_in_nyc, new_neighborhood)
 bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
 new_neighborhood = "Staten Island"
-bouroughs_in_nyc.unshift(new_neighborhood)
+  bouroughs_in_nyc.unshift(new_neighborhood)
 end
 
 def using_pop(continents)
-continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
-continents.pop()
+  continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+  continents.pop()
 end
 
 def pop_with_args(dog_breeds)
-dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
-dog_breeds.pop(2)
+  dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+  dog_breeds.pop(2)
 end
 
 def using_shift(my_favorite_cities)
-my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]  
-my_favorite_cities.shift()
+  my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]  
+  my_favorite_cities.shift()
 end
 
-def shift_with_args(array)
-  
+def shift_with_args(ice_cream_brands)
+  ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
+
 end
 
 def using_concat(array1, array2)
