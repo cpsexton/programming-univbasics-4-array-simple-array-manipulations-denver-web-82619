8330f44aa4479e069f9b9a346dafdc9c1aa68ae3
Christopher Sexton <cpsexton@gmail.com>
2019-08-03 21:43:37 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -55,7 +55,7 @@ end
 def using_delete(instructors, no_offense_steven)
   instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
   no_offense_steven = "Steven"
-  instructors.delete(no_offense_steven)
+  instructors.delete[no_offense_steven]
 end
 
 def using_delete_at(array, integer)
