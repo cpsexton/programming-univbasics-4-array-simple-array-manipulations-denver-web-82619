58bbe7e0fcd3724b77fea1677e1c430aed6f91a0
Christopher Sexton <cpsexton@gmail.com>
2019-08-03 21:00:23 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -36,8 +36,9 @@ def using_concat(my_favorite_things, more_favs)
   my_favorite_things = my_favorite_things.concat(more_favs)
 end
 
-def using_insert(array, new_element)
-  
+def using_insert(list_of_programming_languages, new_element)
+list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
+list_of_programming_languages.insert()
 end
 
 def using_uniq(array)
