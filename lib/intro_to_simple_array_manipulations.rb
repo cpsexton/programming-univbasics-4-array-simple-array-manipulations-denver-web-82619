e9e6dce1bbfd3e777e776995d3d14f92cc5994ca
Christopher Sexton <cpsexton@gmail.com>
2019-08-03 20:52:53 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,12 +1,12 @@
 def using_push(colors_in_the_rainbow, next_color)
-colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
-next_color = "violet"
-colors_in_the_rainbow.push(next_color)
+  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+  next_color = "violet"
+  colors_in_the_rainbow.push(next_color)
 end
 
 def using_unshift(bouroughs_in_nyc, new_neighborhood)
-bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
-new_neighborhood = "Staten Island"
+  bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+  new_neighborhood = "Staten Island"
   bouroughs_in_nyc.unshift(new_neighborhood)
 end
 
@@ -27,7 +27,7 @@ end
 
 def shift_with_args(ice_cream_brands)
   ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
-
+  ice_cream_brands.shift(2)
 end
 
 def using_concat(array1, array2)
