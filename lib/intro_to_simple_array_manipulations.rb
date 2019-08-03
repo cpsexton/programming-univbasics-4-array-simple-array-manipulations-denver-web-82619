07f9e4f52ca7e76dee104300c18c0d43e259e675
Christopher Sexton <cpsexton@gmail.com>
2019-08-03 21:46:47 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -58,6 +58,7 @@ def using_delete(instructors, no_offense_steven)
   instructors.delete["Steven"]
 end
 
-def using_delete_at(famous_robots, integer)
+def using_delete_at(famous_robots, 2)
   famous_robots = ["Johnny 5", "R2D2", "Robocop"]
+  famous_robots.delete_at(2)
 end
