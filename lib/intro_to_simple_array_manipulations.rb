bc3875208f29226dca8adf441c9e4474446db7aa
Christopher Sexton <cpsexton@gmail.com>
2019-08-03 20:58:13 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -22,7 +22,7 @@ end
 
 def using_shift(my_favorite_cities)
   my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]  
-  my_favorite_cities.shift(1)
+  my_favorite_cities.shift()
 end
 
 def shift_with_args(ice_cream_brands)
